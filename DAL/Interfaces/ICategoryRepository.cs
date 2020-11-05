fcd23e1743b56d2974bc47efbd0b921a685adb48
DoThiKhanh <khanhdo22091999@gmail.com>
2020-11-05 13:48:58 +0700

    Update

--- DAL/Interfaces/ICategoryRepository.cs
+++ DAL/Interfaces/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using Models;
+﻿
+using Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,10 @@ namespace DAL.Interfaces
     public partial interface ICategoryRepository
     {
         List<CategoryModel> GetData();
+        CategoryModel GetDatabyID(string id);
+        bool Create(CategoryModel model);
+        bool Update(CategoryModel model);
+        bool Delete(string id);
+        List<CategoryModel> Search(int pageIndex, int pageSize, out long total, string category_name);
     }
 }
-
--- Models/BrandModel.cs
+++ Models/BrandModel.cs
@@ -9,8 +9,8 @@ namespace Models
         public string parent_brand_id { get; set; }
         public string brand_id { get; set; }
         public string brand_name { get; set; }
-        public string seq_num { get; set; }
-        public short? url { get; set; }
+        public string desc { get; set; }
+        public string url { get; set; }
         public List<BrandModel> children { get; set; }
         public string type { get; set; }
     }
